2511ad72f463688dd10cceccdf38e299fff69c7a
Leongum <luyongluy@gmail.com>
2013-11-12 14:26:37 +0800

    add plan init

---
+++ entity/src/main/java/com/usavich/entity/Plan/PlanRunHistory.java
@@ -0,0 +1,122 @@
+package com.usavich.entity.plan;
+
+import com.usavich.common.lib.CustomDateDeserializer;
+import com.usavich.common.lib.CustomDateSerializer;
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: p
+ * Date: 13-11-8
+ * Time: 下午2:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class PlanRunHistory {
+
+    private Integer planId;
+    private String planRunUuid;
+    private Integer nextMissionId;
+    private Integer userId;
+    private Date startTime;
+    private Date endTime;
+    private Integer rate;
+    private String rateComment;
+    private Integer remainingMissions;
+    private Integer totalMissions;
+    private Integer historyStatus; //0 execute 1 finished  -1 cancled
+
+    public Integer getPlanId() {
+        return planId;
+    }
+
+    public void setPlanId(Integer planId) {
+        this.planId = planId;
+    }
+
+    public String getPlanRunUuid() {
+        return planRunUuid;
+    }
+
+    public void setPlanRunUuid(String planRunUuid) {
+        this.planRunUuid = planRunUuid;
+    }
+
+    public Integer getNextMissionId() {
+        return nextMissionId;
+    }
+
+    public void setNextMissionId(Integer nextMissionId) {
+        this.nextMissionId = nextMissionId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    @JsonSerialize(using = CustomDateSerializer.class)
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    @JsonDeserialize(using = CustomDateDeserializer.class)
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    @JsonSerialize(using = CustomDateSerializer.class)
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    @JsonDeserialize(using = CustomDateDeserializer.class)
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public Integer getRate() {
+        return rate;
+    }
+
+    public void setRate(Integer rate) {
+        this.rate = rate;
+    }
+
+    public String getRateComment() {
+        return rateComment;
+    }
+
+    public void setRateComment(String rateComment) {
+        this.rateComment = rateComment;
+    }
+
+    public Integer getRemainingMissions() {
+        return remainingMissions;
+    }
+
+    public void setRemainingMissions(Integer remainingMissions) {
+        this.remainingMissions = remainingMissions;
+    }
+
+    public Integer getTotalMissions() {
+        return totalMissions;
+    }
+
+    public void setTotalMissions(Integer totalMissions) {
+        this.totalMissions = totalMissions;
+    }
+
+    public Integer getHistoryStatus() {
+        return historyStatus;
+    }
+
+    public void setHistoryStatus(Integer historyStatus) {
+        this.historyStatus = historyStatus;
+    }
+}
---
+++ entity/src/main/java/com/usavich/entity/Plan/PlanUserFollow.java
@@ -0,0 +1,66 @@
+package com.usavich.entity.plan;
+
+import com.usavich.common.lib.CustomDateDeserializer;
+import com.usavich.common.lib.CustomDateSerializer;
+import org.codehaus.jackson.map.annotate.JsonDeserialize;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: p
+ * Date: 13-11-8
+ * Time: 下午5:19
+ * To change this template use File | Settings | File Templates.
+ */
+public class PlanUserFollow {
+
+    private Integer userId;
+    private Integer followUserId;
+    private Integer status;
+    private Date addTime;
+    private Integer operate; //1 add 2 update.
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getFollowUserId() {
+        return followUserId;
+    }
+
+    public void setFollowUserId(Integer followUserId) {
+        this.followUserId = followUserId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    @JsonSerialize(using = CustomDateSerializer.class)
+    public Date getAddTime() {
+        return addTime;
+    }
+
+    @JsonDeserialize(using = CustomDateDeserializer.class)
+    public void setAddTime(Date addTime) {
+        this.addTime = addTime;
+    }
+
+    public Integer getOperate() {
+        return operate;
+    }
+
+    public void setOperate(Integer operate) {
+        this.operate = operate;
+    }
+}
